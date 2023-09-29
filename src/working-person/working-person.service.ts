3e74451e0503b5a0de787a55abb873a849148d1d
betrayal <vogistv@gmail.com>
2023-09-30 01:05:18 +0700

    added service working perosn

--- src/working-person/working-person.service.ts
+++ src/working-person/working-person.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {WorkingPerson} from "./working-person.model";
+import {InjectModel} from "@nestjs/sequelize";
 
 @Injectable()
-export class WorkingPersonService {}
+export class WorkingPersonService {
+
+    constructor(@InjectModel(WorkingPerson) private readonly workingPersonRepository: typeof WorkingPerson) {
+    }
+
+    async getAll(): Promise<WorkingPerson[]>{
+        const workingPersons = this.workingPersonRepository.findAll()
+        return workingPersons;
+    }
+
+    async get(id: number): Promise<WorkingPerson>{
+        const workingPerson = this.workingPersonRepository.findByPk(id)
+
+        if(!workingPerson){
+            throw new NotFoundException(`Not found element with id ${id}`)
+        }
+
+        return workingPerson;
+    }
+}
